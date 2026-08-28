d03f252ff032d6ce3405340a3564b7dc91a4a551
Pedro Caio Andrade de Sousa <pedrocaioandrade9@gmail.com>
2026-08-28 15:06:59 -0300

    Coleta de versao

--- api/app.py
+++ api/app.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-pasta_ti = Path(__file__).parent.parent / "version"
+pasta_ti = Path(__file__).parent / "version"
 
 
 @app.route("/resultado", methods=["POST"])
