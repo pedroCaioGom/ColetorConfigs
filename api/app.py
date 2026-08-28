43c16ca4a9eedbcec3373bfeeba0a63085018eeb
Pedro Caio Andrade de Sousa <pedrocaioandrade9@gmail.com>
2026-08-28 08:52:00 -0300

    atualizando api

--- api/app.py
+++ api/app.py
@@ -1,7 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from insert_sheet import insert, search
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 app = Flask(__name__)
+
+pasta_ti = os.getenv("pasta_version")
 
 @app.route("/resultado", methods=["POST"])
 def receber():
@@ -22,12 +30,61 @@ def receber():
     )
     return dados
 
+
 @app.route("/enviar")
 def enviar():
     id_planilha = search()
     return jsonify({
         "id_planilha": id_planilha
     }), 200
+
+
+@app.route("/versao")
+def versao_server():
+
+    caminho_versao = Path(pasta_ti) / "version"
+
+    versao = caminho_versao.read_text().split("=")[1]
+    
+    return (versao), 200
+
+
+@app.route("/update")
+def update():
+    resposta = versao_server()
+    
+    dados = resposta[0]
+   
+    caminho_arquivo = (
+        Path(pasta_ti) / dados
+    )
+    
+    for arq in caminho_arquivo.glob("*.zip"):
+        print("raiz ",caminho_arquivo)
+        print("arquivoZip: ", arq)
+    
+    return send_file(
+        str(arq),
+        as_attachment=True,
+        download_name="arquivosTI.zip",
+        mimetype="application/octet-stream"
+    )
+
+@app.route("/arquivos")
+def verificar_arquivos_exe():
+    resposta = versao_server()
+    
+    dados = resposta[0]
+   
+    caminho_arquivo = (
+        Path(pasta_ti) / dados
+    )
+
+    lista = []
+    for arq in caminho_arquivo.glob("*.exe"):
+        lista.append(arq.name)
+
+    return lista
 
 
 if __name__ == '__main__':
